d9af96cb0fb28d0b68edbef2b265a5cfa245eee4
Fert028 <hack101white@gmail.com>
2024-06-18 16:00:10 +0300

    form is undefuned

--- src/pages/CalcPage/Calc.jsx
+++ src/pages/CalcPage/Calc.jsx
@@ -1,34 +1,43 @@
 import styles from './Calc.module.scss';
 
 function Calc() {
-  const calculator = (event) => {
-    event.preventDefault();
-    document.getElementById('submitCalcButton').addEventListener('click', () => {
-      let indexFloor = document.querySelector('.checkFloor').value;
-      switch (indexFloor) {
-        case 'man': console.log('Выбранно: man'); break;
-        case 'gerl': console.log('Выбранно: gerl'); break;
-      }
-    })
-  }
+  const form = document.forms.calculator;
+  const formInputFloor = form.elements.floor;
+  const formInputAge = form.elements.age;
+  const formInputHeight = form.elements.height;
+  const formInputWeight = form.elements.weight;
+  const submitCalcButton = document.getElementById('submitCalcButton');
+  const outputResult = document.getElementById('outputResult');
+ 
+  submitCalcButton.addEventListener('click', () => {
+    const calculation = ((10 * formInputWeight) + (6.25 * formInputHeight) - (5 * formInputAge));
+    switch (formInputFloor.value) {
+      case 'man': outputResult.innerHTML = calculation + 5;
+      break;
+      case 'gerl': outputResult.innerHTML = calculation - 161;
+      break;
+      default: alert('Заолните все поля');
+      break;
+    }
+  });
 
   return(
     <main>
       <section className={styles.SCalc}>
         <h1>Калькулятор калорий</h1>
 
-        <form className={styles.calculator}>
+        <form className={styles.calculator} name='calculator'>
           
           <div className={styles.formFloor}>
             <h3>Выберите пол:</h3>
             
             <div className={styles.inputFloor}>
-              <input className='checkFloor' id='man' type="radio" name='floor' value='man' />
+              <input className={styles.radioFloor} id='man' type="radio" name='floor' value='man' />
               <label htmlFor="man">Мужской</label>
             </div>
             
             <div className={styles.inputFloor}>
-              <input className='checkFloor' id='gerl' type="radio" name='floor' value='gerl' />
+              <input className={styles.radioFloor} id='gerl' type="radio" name='floor' value='gerl' />
               <label htmlFor="gerl">Женский</label>
             </div>
             
@@ -53,9 +62,11 @@ function Calc() {
 
           </div>
 
-          <button id={styles.submitCalcButton} type='submit' onClick={calculator}>Расчитать каллории</button>
+          <button id={styles.submitCalcButton} type='submit'>Расчитать каллории</button>
 
         </form>
+
+        <h3 id={styles.outputResult}></h3>
 
       </section>
     </main>
